89ea457508caa51feab84711e6a35a34767504ed
stephenrobxrts <stephenrobxrts@gmail.com>
2025-08-12 17:26:21 +1000

    Added Final Damage Score + Cleanup Project

--- PDT Ball Game/Assets/Scripts/PinballScore.cs
+++ PDT Ball Game/Assets/Scripts/PinballScore.cs
@@ -6,10 +6,19 @@ public class PinballScore : MonoBehaviour
     public GameObject floatingTextPrefab;
     private RectTransform uiCanvas;
 
+    private PinballLauncher launcher;
+
     void Awake()
     {
         if (uiCanvas == null)
             uiCanvas = FindFirstObjectByType<Canvas>().GetComponent<RectTransform>();
+    }
+
+    void Start()
+    {
+        GameObject launcherObj = GameObject.FindGameObjectWithTag("Launcher");
+        if (launcherObj != null)
+            launcher = launcherObj.GetComponent<PinballLauncher>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -28,18 +37,37 @@ public class PinballScore : MonoBehaviour
         score += points;
         Debug.Log($"Hit {collision.collider.tag}! Score: {score}");
 
+        ShowFloatingText("+" + points, collision.GetContact(0).point, Color.white);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Exit"))
+        {
+            ShowFloatingText("Total Damage: " + score, transform.position, Color.red);
+
+            if (launcher != null)
+                launcher.UnlockPinballLauncher(true);
+
+            Destroy(gameObject);
+        }
+    }
+
+    private void ShowFloatingText(string text, Vector3 worldPosition, Color color)
+    {
         if (floatingTextPrefab != null && uiCanvas != null)
         {
-            Vector3 hitPos = collision.contactCount > 0 ? collision.GetContact(0).point : collision.transform.position;
-            Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(Camera.main, hitPos);
+            Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(Camera.main, worldPosition);
             GameObject floatingTextGO = Instantiate(floatingTextPrefab, uiCanvas);
             RectTransform floatingRect = floatingTextGO.GetComponent<RectTransform>();
+
             Vector2 localPoint;
             RectTransformUtility.ScreenPointToLocalPointInRectangle(uiCanvas, screenPoint, Camera.main, out localPoint);
             floatingRect.anchoredPosition = localPoint;
+
             FloatingText floatingText = floatingTextGO.GetComponent<FloatingText>();
             if (floatingText != null)
-                floatingText.Setup("+" + points);
+                floatingText.Setup(text);
         }
     }
 }
